a36bc3406ee5fb070f1a67643ed5a7f21b465747
陌上云杉 <2023211476@bupt.cn>
2025-08-11 22:56:22 +0800

    后端设计完了简单的模型

---
+++ backend/posts/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 10,unique = True)
+
+    class Meta:
+        verbose_name= "分类"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 10,unique = True)
+
+    class Meta:
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    context = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_change_time = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="分类")
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        verbose_name="标签"
+        )
+
+    class Meta:
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
