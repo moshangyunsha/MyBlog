a8a9fa3812b4082756d4d2aa48905ee445ee235c
陌上云杉 <2023211476@bupt.cn>
2025-08-17 01:32:00 +0800

    清理前端vite基本样式并简单实现了Post-list

---
+++ fronted/src/components/PostList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="post-list">
+        <h2>文章列表</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.post-list {
+  max-width: 800px;
+  margin: 20px auto;
+  padding: 20px;
+}
+</style>
